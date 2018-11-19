174a3717cc3cc86f9886b8cccab2f8793790de0b
ace512 <marlene.pfefferkorn@haw-hamburg.de>
2018-11-19 11:04:28 +0100

    Daten erweitert

--- examples/RIOTProject/src/gui/daten/Daten.java
+++ examples/RIOTProject/src/gui/daten/Daten.java
@@ -4,5 +4,15 @@ import java.util.ArrayList;
 
 public class Daten {
 
-    ArrayList<Windgeschwindigkeit> windGeschwindigkeit = new ArrayList<>();
+    ArrayList<Windgeschwindigkeit> windGeschwindigkeiten = new ArrayList<>();
+
+    public Daten() {}
+
+    public void addWindgeschwindigkeit(Windgeschwindigkeit windgeschwindigkeit) {
+        this.windGeschwindigkeiten.add(windgeschwindigkeit);
+    }
+
+    public ArrayList<Windgeschwindigkeit> getWindGeschwindigkeiten() {
+        return windGeschwindigkeiten;
+    }
 }
